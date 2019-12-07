e94ffd9d2c36f842c2b96ddf349180bddcc5b642
Joe-noh <goflb.jh@gmail.com>
2019-12-07 13:04:28 +0900

    solve Q3 part2

--- lib/aoc2019/q3.ex
+++ lib/aoc2019/q3.ex
@@ -6,24 +6,32 @@ defmodule AOC2019.Q3 do
   end
 
   def p1(ops_list) do
-    [path1, path2] =
-      ops_list
-      |> Enum.map(fn ops ->
-        ops
-        |> Enum.reduce({[], {0, 0}}, fn op, {coords, origin} ->
-          tracks = [current_coord | _] = tracks(op, origin)
-
-          {tracks ++ coords, current_coord}
-        end)
-      end)
-      |> Enum.map(fn {coords, _} ->
-        Enum.uniq(coords)
-        |> Enum.reduce(%{}, fn coord, acc -> Map.put(acc, coord, true) end)
-      end)
+    [path1, path2] = ops_to_paths(ops_list)
 
     intersection(path1, path2)
     |> Enum.map(fn {x, y} -> abs(x) + abs(y) end)
     |> Enum.min()
+  end
+
+  def p2(ops_list) do
+    [path1, path2] = ops_to_paths(ops_list)
+
+    intersection(path1, path2)
+    |> Enum.map(fn coord -> min_steps(path1, coord) + min_steps(path2, coord) end)
+    |> Enum.min()
+  end
+
+  defp ops_to_paths(ops_list) do
+    ops_list
+    |> Enum.map(fn ops ->
+      {coords, _} = Enum.reduce(ops, {[], {0, 0}}, fn op, {coords, origin} ->
+        tracks = [current_coord | _] = tracks(op, origin)
+
+        {tracks ++ coords, current_coord}
+      end)
+
+      Enum.reverse(coords)
+    end)
   end
 
   defp tracks(op, origin) do
@@ -35,14 +43,20 @@ defmodule AOC2019.Q3 do
     end
   end
 
+  defp min_steps(path, coord) do
+    Enum.find_index(path, &(&1 == coord))  + 1
+  end
+
   defp populate({x, y}, distance, {dx, dy}) do
     Enum.map(1..distance, fn d -> {x + d * dx, y + d * dy} end) |> Enum.reverse()
   end
 
   defp intersection(list1, list2) do
-    list1
-    |> Map.keys()
-    |> Enum.filter(fn key -> Map.get(list2, key) end)
+    [_map1, map2] = Enum.map([list1, list2], fn list ->
+      Enum.uniq(list) |> Enum.reduce(%{}, & Map.put(&2, &1, true))
+    end)
+
+    list1 |> Enum.filter(fn key -> Map.get(map2, key) end)
   end
 
   defp parse_int(str) do
--- test/aoc2019/q3_test.exs
+++ test/aoc2019/q3_test.exs
@@ -1,8 +1,8 @@
 defmodule AOC2019.Q3Test do
   use ExUnit.Case, async: true
 
-  describe "example" do
-    test "1" do
+  describe "p1" do
+    test "example 1" do
       ops_list = [
         ~w[U7 R6 D4 L4],
         ~w[R8 U5 L5 D3]
@@ -11,7 +11,7 @@ defmodule AOC2019.Q3Test do
       assert 6 == AOC2019.Q3.p1(ops_list)
     end
 
-    test "2" do
+    test "example 2" do
       ops_list = [
         ~w[R75 D30 R83 U83 L12 D49 R71 U7 L72],
         ~w[U62 R66 U55 R34 D71 R55 D58 R83]
@@ -20,7 +20,7 @@ defmodule AOC2019.Q3Test do
       assert 159 == AOC2019.Q3.p1(ops_list)
     end
 
-    test "3" do
+    test "example 3" do
       ops_list = [
         ~w[R98 U47 R26 D63 R33 U87 L62 D20 R33 U53 R51],
         ~w[U98 R91 D20 R16 D67 R40 U7 R15 U6 R7]
@@ -28,11 +28,42 @@ defmodule AOC2019.Q3Test do
 
       assert 135 == AOC2019.Q3.p1(ops_list)
     end
-  end
 
-  describe "p1" do
     test "puts", context do
       AOC2019.Q3.input() |> AOC2019.Q3.p1() |> TestHelpers.puts(context)
+    end
+  end
+
+  describe  "p2" do
+    test "example 1" do
+      ops_list = [
+        ~w[U7 R6 D4 L4],
+        ~w[R8 U5 L5 D3]
+      ]
+
+      assert 30 == AOC2019.Q3.p2(ops_list)
+    end
+
+    test "example 2" do
+      ops_list = [
+        ~w[R75 D30 R83 U83 L12 D49 R71 U7 L72],
+        ~w[U62 R66 U55 R34 D71 R55 D58 R83]
+      ]
+
+      assert 610 == AOC2019.Q3.p2(ops_list)
+    end
+
+    test "example 3" do
+      ops_list = [
+        ~w[R98 U47 R26 D63 R33 U87 L62 D20 R33 U53 R51],
+        ~w[U98 R91 D20 R16 D67 R40 U7 R15 U6 R7]
+      ]
+
+      assert 410 == AOC2019.Q3.p2(ops_list)
+    end
+
+    test "puts", context do
+      AOC2019.Q3.input() |> AOC2019.Q3.p2() |> TestHelpers.puts(context)
     end
   end
 end
