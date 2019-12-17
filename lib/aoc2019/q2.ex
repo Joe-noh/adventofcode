76f3f8e106798db02ebbf77385a1171e68d59f43
Joe-noh <goflb.jh@gmail.com>
2019-12-18 05:57:37 +0900

    solve Q5 part1

--- lib/aoc2019/q2.ex
+++ lib/aoc2019/q2.ex
@@ -1,6 +1,8 @@
 defmodule AOC2019.Q2 do
+  alias AOC2019.Intcode
+
   def p1 do
-    input() |> AOC2019.Intcode.calc({12, 2})
+    input() |> Intcode.new() |> Intcode.replace({12, 2}) |> Intcode.calc()
   end
 
   def p2 do
@@ -10,7 +12,7 @@ defmodule AOC2019.Q2 do
     pairs = for month <- 0..99, day <- 0..99, into: [], do: {month, day}
 
     {noun, verb} = Enum.find(pairs, fn pair ->
-      19690720 == AOC2019.Intcode.calc(codes, pair)
+      19690720 == Intcode.new(codes) |> Intcode.replace(pair) |> Intcode.calc()
     end)
 
     noun * 100 + verb
---
+++ lib/aoc2019/q5.ex
@@ -0,0 +1,17 @@
+defmodule AOC2019.Q5 do
+  alias AOC2019.Intcode
+
+  def p1 do
+    input() |> Intcode.new() |> Intcode.input(1) |> Intcode.calc()
+  end
+
+  defp input() do
+    AOC.Input.read!(2019, 5)
+    |> String.split(",")
+    |> Enum.map(&parse_int/1)
+  end
+
+  defp parse_int(str) do
+    str |> Integer.parse() |> elem(0)
+  end
+end
