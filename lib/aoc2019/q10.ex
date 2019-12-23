3b01479332ff28866b5c0efee5e2d1d38658b1bd
Joe-noh <goflb.jh@gmail.com>
2019-12-23 21:31:54 +0900

    solve Q10 part 2

--- lib/aoc2019/q10.ex
+++ lib/aoc2019/q10.ex
@@ -1,19 +1,46 @@
 defmodule AOC2019.Q10 do
   def p1(lines) do
     map = asteroids_map(lines)
-    asteroid_coords = Enum.reduce(map, [], fn
-      {coord, true}, acc -> [coord | acc]
-      {_, false}, acc -> acc
-    end)
+    coords = asteroid_coords(map)
 
-    asteroid_coords
-    |> Enum.map(fn coord ->
-      asteroid_coords
-      |> Enum.map(fn asteroid_coord -> direction(coord, asteroid_coord) end)
-      |> Enum.uniq()
-      |> Enum.count(fn direction -> detect_asteroid?(map, coord, direction) end)
-    end)
+    coords
+    |> Enum.map(fn coord -> detectable_count(map, coords, coord) end)
     |> Enum.max()
+  end
+
+  def p2(lines) do
+    map = asteroids_map(lines)
+    coords = asteroid_coords(map)
+
+    station_coord = Enum.max_by(coords, fn coord -> detectable_count(map, coords, coord) end)
+    map = Map.put(map, station_coord, false)
+
+    directions =
+      coords
+      |> Enum.filter(fn coord -> coord != station_coord end)
+      |> Enum.map(fn coord -> direction(station_coord, coord) end)
+      |> Enum.uniq()
+      |> Enum.sort_by(fn coord -> angle(coord) end)
+
+    directions_length = length(directions)
+    directions_map = directions |> Enum.with_index() |> Enum.map(fn {d, i} -> {i, d} end) |> Enum.into(%{})
+
+    {x, y} =
+      Enum.reduce_while(1..10000, {nil, 0, 0, map}, fn _i, {_vaporized, count, index, acc} ->
+        direction = Map.get(directions_map, rem(index, directions_length))
+
+        case vaporize(acc, station_coord, direction) do
+          {nil, map} -> {:cont, {nil, count, index+1, map}}
+          {vaporized, map} ->
+            if count == 199 do
+              {:halt, vaporized}
+            else
+              {:cont, {vaporized, count+1, index+1, map}}
+            end
+        end
+      end)
+
+    100*x + y
   end
 
   defp asteroids_map(lines) do
@@ -31,6 +58,20 @@ defmodule AOC2019.Q10 do
     end)
   end
 
+  defp asteroid_coords(map) do
+    Enum.reduce(map, [], fn
+      {coord, true}, acc -> [coord | acc]
+      {_, false}, acc -> acc
+    end)
+  end
+
+  defp detectable_count(asteroid_map, asteroid_coords, coord) do
+    asteroid_coords
+    |> Enum.map(fn asteroid_coord -> direction(coord, asteroid_coord) end)
+    |> Enum.uniq()
+    |> Enum.count(fn direction -> detect_asteroid?(asteroid_map, coord, direction) end)
+  end
+
   defp direction({x, y}, {x, y}), do: {0, 0}
 
   defp direction({x1, y1}, {x2, y2}) do
@@ -39,6 +80,10 @@ defmodule AOC2019.Q10 do
 
     {div(x, gcd), div(y, gcd)}
   end
+
+  defp angle({0, y}) when y < 0, do: 0.0
+  defp angle({0, y}) when 0 < y, do: 180.0
+  defp angle({x, y}), do: :math.atan2(-x, y) * 180 / :math.pi() + 180.0
 
   defp detect_asteroid?(_asteroid_map, _from, {0, 0}), do: false
 
@@ -50,5 +95,19 @@ defmodule AOC2019.Q10 do
         nil -> {:halt, false}
       end
     end)
+  end
+
+  defp vaporize(asteroid_map, from, {dx, dy}) do
+    Enum.reduce_while(1..10000, from, fn _, {x, y} ->
+      case Map.get(asteroid_map, {x, y}) do
+        true -> {:halt, {x, y}}
+        false -> {:cont, {x + dx, y + dy}}
+        nil -> {:halt, nil}
+      end
+    end)
+    |> case do
+      nil -> {nil, asteroid_map}
+      coord -> {coord, Map.put(asteroid_map, coord, false)}
+    end
   end
 end
