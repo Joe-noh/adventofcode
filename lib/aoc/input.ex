ae3573c43abea5ee2ce48e957d89ebefa1adea70
Joe-noh <goflb.jh@gmail.com>
2020-12-02 01:01:19 +0900

    add read_as_numbers!/2 helper function

--- lib/aoc/input.ex
+++ lib/aoc/input.ex
@@ -3,6 +3,13 @@ defmodule AOC.Input do
     path(year, num) |> File.read!()
   end
 
+  def read_as_numbers!(year, num) do
+    path(year, num)
+    |> File.read!()
+    |> String.split()
+    |> Enum.map(fn e -> Integer.parse(e) |> elem(0) end)
+  end
+
   def stream!(year, num) do
     path(year, num) |> File.stream!([], :line)
   end
